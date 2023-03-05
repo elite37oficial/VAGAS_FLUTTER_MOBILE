a355802d89adc659914159ff3ad356e277ec8cdd
TeofiloAlbuquerque <contatoteofilosantos@gmail.com>
2023-03-05 09:50:13 -0300

    Layout da WelcomePage concluido, faltando apenas finalizar a responsividade e imagens SVG inseridas

---
+++ lib/core/ui/styles/app_colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color greyBlue = Color(0XFF387CA6);
+  static const Color darkBlue = Color(0XFF023E73);
+  static const Color grey = Color(0XFFE4EDF2);
+  static const Color lightBlue = Color(0XFF43BDD9);
+}
--- lib/views/pages/welcome_page.dart
+++ lib/views/pages/welcome_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import '../../core/ui/styles/app_colors.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class WelcomePage extends StatelessWidget {
   const WelcomePage({super.key});
@@ -9,77 +11,98 @@ class WelcomePage extends StatelessWidget {
 
     return Scaffold(
       backgroundColor: const Color(0xFF387CA6),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            SizedBox(height: mediaQuery.size.height * .1),
-            Image.asset(
-              'assets/images/logo_elite_37.png',
-              height: mediaQuery.size.height * .5,
-              width: mediaQuery.size.width * .7,
-            ),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Padding(
-                  padding:
-                      const EdgeInsets.only(left: 35, right: 35, bottom: 20),
-                  child: Text(
-                    'EliteVagas',
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 22,
-                      color: Colors.grey[100],
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Stack(
+                alignment: AlignmentDirectional.center,
+                children: <Widget>[
+                  Positioned(
+                    child: SvgPicture.asset(
+                      'assets/images/background_icons.svg',
+                      height: mediaQuery.size.height * .6,
+                      width: mediaQuery.size.width * .9,
                     ),
                   ),
-                ),
-                Padding(
-                  padding:
-                      const EdgeInsets.only(left: 35, right: 35, bottom: 20),
-                  child: Text(
-                    'Encontre aqui as melhores vagas de Flutter!',
-                    style: TextStyle(
+                  const Positioned(
+                    bottom: 3,
+                    left: 0,
+                    child: Text(
+                      'EliteVagas',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
                         fontSize: 22,
-                        color: Colors.grey[100],
-                        fontWeight: FontWeight.bold),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(
-                    left: 40,
-                    right: 40,
-                  ),
-                  child: Text(
-                    'Aqui na EliteVagas você terá acesso direto com os recrutadores',
-                    style: TextStyle(
-                      color: Colors.grey[100],
+                        color: AppColors.grey,
+                      ),
                     ),
                   ),
-                ),
-              ],
-            ),
-            const SizedBox(
-              height: 60,
-            ),
-            SizedBox(
-              height: mediaQuery.size.height * .06,
-              width: mediaQuery.size.width * .8,
-              child: ElevatedButton(
-                onPressed: () {},
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.grey[100],
-                ),
-                child: const Text(
-                  'Ver vagas',
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 16,
+                  Positioned(
+                    bottom: 70,
+                    child: SvgPicture.asset(
+                      'assets/images/logo_elite_37.svg',
+                      height: mediaQuery.size.height * 0.36,
+                      width: mediaQuery.size.width * .4,
+                    ),
+                  ),
+                ],
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: const <Widget>[
+                  Padding(
+                    padding: EdgeInsets.only(
+                      left: 35,
+                      right: 35,
+                      bottom: 10,
+                      top: 20,
+                    ),
+                    child: Text(
+                      'Encontre aqui as melhores vagas de Flutter!',
+                      style: TextStyle(
+                        fontSize: 22,
+                        color: AppColors.grey,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(
+                      left: 40,
+                      right: 40,
+                      bottom: 40,
+                    ),
+                    child: Text(
+                      "Aqui na EliteVagas você terá acesso\n"
+                      "direto com os recrutadores",
+                      style: TextStyle(
+                        color: AppColors.grey,
+                        fontSize: 15,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: mediaQuery.size.height * .06,
+                width: mediaQuery.size.width * .8,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: AppColors.grey,
+                  ),
+                  child: const Text(
+                    'Ver vagas',
+                    style: TextStyle(
+                      color: AppColors.greyBlue,
+                      fontSize: 17,
+                    ),
                   ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
