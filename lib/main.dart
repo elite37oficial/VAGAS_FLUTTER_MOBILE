c1da08940b94bd80d13094654868a41fca4dd20a
Wellyson Pontes <wellysonpontes@MacBook-Pro-de-Wellyson.local>
2023-03-14 00:11:04 -0300

    revisado conflitos da moficação proposta por Teófilo na tela de detalhes de vagas

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
 import 'my_app.dart';
 
-import 'views/pages/vacancy_details_page.dart';
-import 'views/pages/welcome_page.dart';
-
 void main() {
   runApp(const MyApp());
 }
--- lib/views/home/home_view.dart
+++ lib/views/home/home_view.dart
@@ -12,106 +12,75 @@ class HomeView extends StatelessWidget {
   Widget build(BuildContext context) {
     final List<JobModel> jobsList = mockJob;
     return Scaffold(
-        appBar: AppBar(
-          title: const Text(
-            "Elite Vagas",
-            style: TextStyle(fontSize: 32),
-          ),
-          backgroundColor: AppColors.appBar,
-          elevation: 0,
+      appBar: AppBar(
+        title: const Text(
+          "Elite Vagas",
+          style: TextStyle(fontSize: 32),
         ),
-        body: CustomScrollView(
-          slivers: [
-            SliverAppBar(
-              backgroundColor: AppColors.appBar,
-              title: const Text(
-                "Busque pela vaga ideal...",
-                style: TextStyle(color: Colors.black),
-              ),
-              flexibleSpace: FlexibleSpaceBar(
-                background: Align(
-                  alignment: Alignment.center,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      const SizedBox(),
-                      Container(
-                        color: Colors.white,
-                        padding: const EdgeInsets.only(
-                            left: 22, right: 22, top: 38, bottom: 24),
-                        child: Row(
-                          crossAxisAlignment: CrossAxisAlignment.end,
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Text(
-                              "Vagas",
-                              style: context.textStyles.textTitleCards,
-                            ),
-                            Row(
-                              children: [
-                                Text(
-                                  "Filtros",
-                                  style: context.textStyles.textFilterCards,
-                                ),
-                                const SizedBox(width: 8),
-                                Icon(
-                                  Icons.tune_rounded,
-                                  color: AppColors.cardFilter,
-                                ),
-                              ],
-                            ),
-                          ],
-                        ),
+        backgroundColor: AppColors.appBar,
+        elevation: 0,
+      ),
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            backgroundColor: AppColors.appBar,
+            title: const Text(
+              "Busque pela vaga ideal...",
+              style: TextStyle(color: Colors.black),
+            ),
+            flexibleSpace: FlexibleSpaceBar(
+              background: Align(
+                alignment: Alignment.center,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    const SizedBox(),
+                    Container(
+                      color: Colors.white,
+                      padding: const EdgeInsets.only(
+                          left: 22, right: 22, top: 38, bottom: 24),
+                      child: Row(
+                        crossAxisAlignment: CrossAxisAlignment.end,
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            "Vagas",
+                            style: context.textStyles.textTitleCards,
+                          ),
+                          Row(
+                            children: [
+                              Text(
+                                "Filtros",
+                                style: context.textStyles.textFilterCards,
+                              ),
+                              const SizedBox(width: 8),
+                              Icon(
+                                Icons.tune_rounded,
+                                color: AppColors.cardFilter,
+                              ),
+                            ],
+                          ),
+                        ],
                       ),
-                    ],
-                  ),
+                    ),
+                  ],
                 ),
               ),
-              expandedHeight: 150,
-              floating: true,
-              pinned: false,
             ),
-            SliverList(
-              delegate: SliverChildBuilderDelegate(
-                  childCount: jobsList.length,
-                  (context, index) => Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 22),
-                        child: CardWidget(job: jobsList[index]),
-                      )),
-            ),
-          ],
-        )
-
-        // Column(
-        //   children: [
-        //     Padding(
-        //       padding: const EdgeInsets.symmetric(horizontal: 32, vertical: 17),
-        //       child: Row(
-        //         mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        //         children: [
-        //           Text(
-        //             "Vagas",
-        //             style: context.textStyles.textTitleCards,
-        //           ),
-        //           const SizedBox(
-        //             width: 25.28,
-        //             height: 20,
-        //             child: Icon(
-        //               Icons.tune,
-        //             ),
-        //           ),
-        //         ],
-        //       ),
-        //     ),
-        //     Expanded(
-        //       child: Padding(
-        //         padding: const EdgeInsets.symmetric(horizontal: 32),
-        //         child: CustomListViewBuilder(
-        //             customSliverAppBar: Text("data"), itemCount: jobsList.length),
-        //       ),
-        //     )
-        //   ],
-        // ),
-        );
+            expandedHeight: 150,
+            floating: true,
+            pinned: false,
+          ),
+          SliverList(
+            delegate: SliverChildBuilderDelegate(
+                childCount: jobsList.length,
+                (context, index) => Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 22),
+                      child: CardWidget(job: jobsList[index]),
+                    )),
+          ),
+        ],
+      ),
+    );
   }
 }
